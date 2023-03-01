956e6f689858fb3597789372a6757f9755cbcda0
Amirhon <amirhonmalikov10@gmail.com>
2023-03-01 13:26:30 +0500

    first js homework

---
+++ main.js
@@ -0,0 +1,35 @@
+// задание №1
+
+var userName = 'Amir';
+var word = prompt('Укажите ' + 'свое ' + 'имя ');
+console.log('Меня ' + 'зовут ' + userName);
+var myAge = 15; 
+var word = prompt('Укажите ' + 'свой ' + 'возраст ');
+console.log('Мне ' + myAge );
+
+
+var word = prompt('Пример 1: ' + '7 + 2 = ?');
+alert(word);
+console.log('Пример 1: ' + '7 + 2 = ' + (7 + 2));
+var word = prompt('Пример 2: ' + '7 - 2 = ?');
+alert(word);
+console.log('Пример 2: ' + '7 - 2 = ' + (7 - 2));
+var word = prompt('Пример 3: ' + '8 / 2 = ?');
+alert(word);
+console.log('Пример 3: ' + '8 / 2 = ' + 8 / 2);
+var word = prompt('Пример 4: ' + '8 * 2 = ?');
+alert(word);
+console.log('Пример 4: ' + '8 * 2 = ' + 8 * 2);
+var word = prompt('Пример 5: ' + '9 % 2 = ?');
+alert(word);
+console.log('Пример 5: ' + '9 % 2 = ' + 9 % 2);
+
+var word = alert('Откройте ' + 'консоль ' + 'для ' + 'просмотра ');
+
+// задание №2
+
+var x = +prompt("3");
+var y = +prompt("4");
+var z = +prompt("5");
+var number = (x+z+y) / 3;
+alert("Среднее арефмитическое число это: " + number);
